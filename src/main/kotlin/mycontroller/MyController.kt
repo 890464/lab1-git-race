73857a85a89cdb28ac94c6922d330c593b7601fe
890464 <127861434+890464@users.noreply.github.com>
2023-03-17 10:06:54 +0100

    Update MyController.kt

--- src/main/kotlin/mycontroller/MyController.kt
+++ src/main/kotlin/mycontroller/MyController.kt
@@ -7,16 +7,8 @@ import org.springframework.web.bind.annotation.RestController
 class MyController {
 
     @GetMapping("/hello")
-            /**
-             *  The value parameter is a special case in which
-             *  its value can be specified without an explicit
-             *  name. In this case, the value given to this
-             *  parameter is the content of the app.message.
-             *  This message is located at /resources/application.properties
-             *  and contains the text "Hola estudiante".
-             */
-    fun sayHello(): String {
+    fun hello(): String {
         return "Hello, World!"
     }
-    
 }
+
